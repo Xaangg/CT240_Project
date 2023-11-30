4818b607532184014f7ea981dcd1b4328846e06a
MinhSang <139788176+Xaangg@users.noreply.github.com>
2023-11-30 17:51:40 +0700

    Update Group.java

--- Code Java/Group.java
+++ Code Java/Group.java
@@ -1,6 +1,6 @@
 /***********************************************************************
  * Module:  Group.java
- * Author:  Lenovo Slim 3
+ * Author:  Vo Minh Sang
  * Purpose: Defines the Class Group
  ***********************************************************************/
 
